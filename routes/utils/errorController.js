abfb04ffc509de1aee068c6c4f05a74365887ccf
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-07-19 11:53:22 -0400

    fixed an issue with passing argument into mongodbduplicate function.

--- routes/utils/errorController.js
+++ routes/utils/errorController.js
@@ -1,7 +1,5 @@
 const ErrorMessageHandlerClass = require("./ErrorMessageHandlerClass");
 
-function handleMongoDBDuplicate(error) {}
-
 function dispatchErrorDevelopment(error, req, res) {
   if (req.originalUrl.startsWith("/api")) {
     return res.status(error.statusCode).json({
@@ -32,19 +30,17 @@ function dispatchErrorProduction(error, req, res) {
 }
 
 function handleMongoDBDuplicate(error) {
-  let errorMessageDuplicateKey = Object.keys(err.keyValue)[0];
+  let errorMessageDuplicateKey = Object.keys(error.keyValue)[0];
 
-  let errorMessageDuplicateValue = Object.values(err.keyValue)[0];
-  let message = `${errorMessageDuplicateKey} - ${errorMessageDuplicatValue} is taken. Please choose another one.`;
+  let errorMessageDuplicateValue = Object.values(error.keyValue)[0];
+  let message = `${errorMessageDuplicateKey} - ${errorMessageDuplicateValue} is taken. Please choose another one.`;
   return new ErrorMessageHandlerClass(message, 400);
 }
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-
   let error = { ...err };
-
   error.message = err.message;
 
   if (error.code === 11000 || error.code === 11001) {
